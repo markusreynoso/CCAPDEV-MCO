2c645f49ce7f5d2450c2e1acb2a7048781354664
wendellabino07 <130436234+wendellabino07@users.noreply.github.com>
2025-01-30 21:04:37 +0800

    Created a modal for edit bio - Editing bio will make no updates

---
+++ bio.js
@@ -0,0 +1,6 @@
+function updateBio(){
+    let bio = document.getElementById("bioContent");
+    let textArea = document.getElementById("bio-text-area");
+    textArea.innerHTML = bio.textContent;
+
+}
