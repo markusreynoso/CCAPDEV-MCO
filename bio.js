011a672df0d86c9e61b9da5ee336e8bbb9084625
markusreynoso <markus.reynoso.c@gmail.com>
2025-02-01 18:12:39 +0800

    Fix indentation issues in bio editing modal

--- bio.js
+++ bio.js
@@ -1,6 +1,5 @@
 function updateBio(){
     let bio = document.getElementById("bioContent");
     let textArea = document.getElementById("bio-text-area");
-    textArea.innerHTML = bio.textContent;
-
+    textArea.value = bio.textContent.replace(/\s+/g, ' ').trim();
 }
