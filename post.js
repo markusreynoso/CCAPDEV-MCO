16e54a0a906793c7471e72531df456d167f650ee
markusreynoso <markus.reynoso.c@gmail.com>
2025-02-06 08:38:34 +0800

    Add delete post and comment modals

--- post.js
+++ post.js
@@ -1,6 +1,9 @@
 function editPost(){
     let modalTextArea = document.getElementById("edit-post-text-area");
+    let modalTitleArea = document.getElementById("edit-post-title-area");
+    let postTitle = document.querySelector(".post-title").textContent.replace(/\s+/g, ' ').trim();
     let postText = document.querySelector(".post-body-text").textContent.replace(/\s+/g, ' ').trim();
+    modalTitleArea.value = postTitle;
     modalTextArea.value = postText;
 }
 
