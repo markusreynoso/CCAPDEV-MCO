2130b32f3a0498def972b4c45142931de84f66b9
markusreynoso <markus.reynoso.c@gmail.com>
2025-02-25 10:32:29 +0800

    Finish comments section handlebars

--- app.js
+++ app.js
@@ -22,59 +22,56 @@ const databaseURL = "mongodb://127.0.0.1:27017/";
 const mongoClient = new MongoClient(databaseURL);
 
 // Main
+
 const databaseName = "MCO";
 
-async function initialConnection(){
+async function initialConnection() {
     let con = await mongoClient.connect();
-    // console.log("Attempt to create!");
     const dbo = mongoClient.db(databaseName);
-    /* Will create a collection if it has not yet been made */
-    // dbo.createCollection(collectionName);
-  }
+}
+
 initialConnection();
 
-server.get('/', async function(req, resp){
+
+server.get('/', function (req, resp) {
+    resp.redirect('/home-unlogged');
+})
+
+server.get('/home-:isLogged', async function (req, resp) {
     const dbo = mongoClient.db(databaseName);
     const postsCollection = await dbo.collection("posts").find().toArray();
+    let isLogged = (req.params.isLogged === "logged");
     resp.render('home', {
         layout: 'index',
         title: 'AskAway - Home',
-        logged: false,
+        logged: isLogged,
         posts: postsCollection
-    })
+    });
 })
 
-server.get('/home-logged', async function(req, resp){
-    const dbo = mongoClient.db(databaseName);
-    const postsCollection = await dbo.collection("posts").find().toArray();
-    resp.render('home', {
-        layout: 'index',
-        title: 'AskAway - Home',
-        logged: true,
-        posts: postsCollection
-    })
-})
-
-server.get('/login', function(req, resp){
+server.get('/login', function (req, resp) {
     resp.render('login', {
         layout: 'index',
         title: 'AskAway - Login',
     })
 })
 
-server.get('/register', function(req, resp){
+server.get('/register', function (req, resp) {
     resp.render('register', {
         layout: 'index',
         title: 'AskAway - Register',
     })
 })
 
-server.get('/post/:isLogged', function(req, resp){
+server.get('/post-:isLogged', async function (req, resp) {
+    const dbo = mongoClient.db(databaseName);
     let isLogged = (req.params.isLogged === "logged");
-    resp.render('login', {
+    const commentsCollection = await dbo.collection("comments").find().toArray();
+    resp.render('post', {
         layout: 'index',
         title: 'View Post',
-        logged: isLogged
+        logged: isLogged,
+        commentsCollection: commentsCollection[0]
     })
 })
 
