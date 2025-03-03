41bf0364b453ca088f3f9fb1c8e5f6e2fc270b06
Macky Abiño <macky.abino@gmail.com>
2025-03-03 23:44:28 +0800

    Finished 1.1 and 1.2
    1.1. Fix editing of replies and comments (JS)
    1.2. Edit bio JS; retrieve current bio

--- public/interactions-logged.js
+++ public/interactions-logged.js
@@ -78,4 +78,30 @@ $(document).ready(function () {
         
         $("#edit-reply-comment-text-area").val(`@${replyingTo} `)
     });
+
+    // edit comment logic
+    $(".post-edit-flag").click(function() {
+        let id = $(this).attr("value");
+        let element = $(".card-body[value='" + id + "']") // $(".card-body[value='67bc87de16350e278a7aaaad']")
+                        .find('p.post-body-text');
+
+        let isComment = element.children().length === 0;
+        let content;
+        if (isComment) {
+            content = element.html().trim();
+        }
+
+        else {
+            let mention = element.children().find("a").html().trim();
+            content = mention + " " + element.clone().children().remove().end().text().trim();
+        }
+        
+        $("#edit-comment-text-area").html(content);
+    });
+
+    $("#edit-bio-btn").click(function() {
+        let bio = $('#bioContent').text().trim();
+        $("#bio-text-area").html(bio);
+
+    });
 })
