54f63306b3849f23b511aca7928410601d4e9164
markusreynoso <markus.reynoso.c@gmail.com>
2025-03-03 17:27:59 +0800

    Fix bug where unlogged user can upvote and downvote

--- public/commentcard.js
+++ public/commentcard.js
@@ -1,43 +1,43 @@
-function createCommentCardBody(author, body) {
-    let cardBody = document.createElement('div');
-    addClass(cardBody, "card-body");
+// function createCommentCardBody(author, body) {
+//     let cardBody = document.createElement('div');
+//     addClass(cardBody, "card-body");
 
-    cardBody.appendChild(createHeader(author));
+//     cardBody.appendChild(createHeader(author));
 
-    cardBody.appendChild(createBody(body));
+//     cardBody.appendChild(createBody(body));
 
-    cardBody.appendChild(createVoteArea());
+//     cardBody.appendChild(createVoteArea());
 
-    return cardBody;
-}
+//     return cardBody;
+// }
 
-function createCommentCard(author, body) {
-    author = 'LuisDaBeast'; //Hardcoded for now; delete later on
-    let card = document.createElement('div');
+// function createCommentCard(author, body) {
+//     author = 'LuisDaBeast'; //Hardcoded for now; delete later on
+//     let card = document.createElement('div');
 
-    addClass(card, "card post");
+//     addClass(card, "card post");
 
-    card.appendChild(createCommentCardBody(author, body));
+//     card.appendChild(createCommentCardBody(author, body));
 
-    let posts = document.getElementById('comments');
-    posts.insertBefore(card, posts.firstChild);
-}
+//     let posts = document.getElementById('comments');
+//     posts.insertBefore(card, posts.firstChild);
+// }
 
-function hardcodedCreateCommentCard() {
-    let author = 'LuisDaBeast'; //Hardcoded for now; delete later on
-    let body = document.getElementById('write-text-area').value;
-    let card = document.createElement('div');
+// function hardcodedCreateCommentCard() {
+//     let author = 'LuisDaBeast'; //Hardcoded for now; delete later on
+//     let body = document.getElementById('write-text-area').value;
+//     let card = document.createElement('div');
 
-    addClass(card, "card post");
+//     addClass(card, "card post");
 
-    card.appendChild(createCommentCardBody(author, "", body));
+//     card.appendChild(createCommentCardBody(author, "", body));
 
-    let comments = document.getElementById('comments');
-    console.log(comments.firstChild);
-    posts.insertBefore(card, comments.firstChild);
+//     let comments = document.getElementById('comments');
+//     console.log(comments.firstChild);
+//     posts.insertBefore(card, comments.firstChild);
     
 
-    clearTextAreas(['write-title-area', 'write-text-area']);
-}
+//     clearTextAreas(['write-title-area', 'write-text-area']);
+// }
 
  
--- public/write.js
+++ public/write.js
@@ -1,6 +1,6 @@
-function clearTextAreas(elementIdList) {
-    for (let i = 0; i < elementIdList.length; i++){
-        let elementId = elementIdList[i];
-        document.getElementById(elementId).value = '';
-    }
-}
+// function clearTextAreas(elementIdList) {
+//     for (let i = 0; i < elementIdList.length; i++){
+//         let elementId = elementIdList[i];
+//         document.getElementById(elementId).value = '';
+//     }
+// }
