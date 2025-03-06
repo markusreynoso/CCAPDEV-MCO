e93129d032b677798a4d27f717a0bfb9b336e200
markusreynoso <markus.reynoso.c@gmail.com>
2025-03-06 21:43:53 +0800

    Add tag editing

--- public/post-edit.js
+++ public/post-edit.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
     $(".bi-pencil").click(function(){
         let parent = $(this).closest(".post");
         let postContent = parent.find("p.post-body-text").text().trim();
+        let postTag = parent.find("div.tag p").text();
         let postTitle = parent.find("h2.post-title").text().trim();
         
         if ($(this).hasClass("post-edit-flag")){
             $("#edit-post-text-area").val(postContent);
             $("#edit-post-title-area").val(postTitle);
+            $("#edit-post-tag-area").val(postTag);
         }
     });
 })
