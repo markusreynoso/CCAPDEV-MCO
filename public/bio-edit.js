ab70012015ff3ed04965fc99e62a04991245c4df
wendellabino07 <130436234+wendellabino07@users.noreply.github.com>
2025-03-29 20:52:30 +0800

    change-bio alert converted to toast - todo change-bio in user-comments is not working for some reason

--- public/bio-edit.js
+++ public/bio-edit.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+
+    function showToast(message, type = "danger") {
+        let toastElement = $("#bioToast");
+
+        toastElement.removeClass("text-bg-danger text-bg-success").addClass("custom-toast");
+        
+        if (type === "success") {
+            toastElement.css("background-color", "var(--blue)");
+        } else {
+            toastElement.css("background-color", "var(--raspberry)");
+        }
+
+        toastElement.find(".toast-body").text(message);
+        
+        let toast = new bootstrap.Toast(toastElement[0], { autohide: true, delay: 3000 });
+        toast.show();
+    }
+
     
     $("#edit-bio-btn").click(function () {
         let bio = $('#bioContent').text().trim().replace(/\s+/g, ' ');
@@ -11,6 +29,11 @@ $(document).ready(function () {
 
         let newBio = $("#bio-text-area").val();
         
+        if (newBio.length > 500) {
+            showToast("Max 500 characters");
+            return;
+        }
+
         $.ajax({
             url: "/change-bio",
             type: "PUT",
@@ -18,12 +41,15 @@ $(document).ready(function () {
             contentType: "application/json",
             success: function( response ) {
                 if (response.success) {
-                    window.location.href = response.redirectUrl;
+                    showToast("Password Successfully Changed!", "success");  
+                    setTimeout(() => {
+                        window.location.href = response.redirectUrl;
+                    }, 1000);
                 }
             },
             error: function (xhr, status, error) {
-                console.error("Error updating username", error);
-                alert("Failed to change username.")
+                console.error("Error updating bio", error);
+                showToast("Failed to change bio.");
             }
         })
     });
@@ -33,3 +59,5 @@ $(document).ready(function () {
         $("#username-text-area").val(currUsername)
     });
 });
+
+
